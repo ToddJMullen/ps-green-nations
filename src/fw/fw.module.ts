c12da92f9b0c7d98c985c14d872332ed0af301d3
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-08-02 21:59:56 -0400

    app.component.html, Replace generated markup with <fw-framework> fw.module.ts, Declare, import & export  fw-framework-body component

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [
+	  FrameworkBodyComponent
+  ]
+  ,exports: [
+	  FrameworkBodyComponent
+  ]
 })
 export class FwModule { }
