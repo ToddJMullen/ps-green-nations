74feced4c3a494c6030d0af411f844553c545254
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-03 08:15:53 -0400

    app.component.ts,  Setup Framework configuration & settings.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+import { FrameworkConfigService, FrameworkConfigSettings } from '../fw/services/framework-config.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  
+  constructor( private appConfigService:FrameworkConfigService ){
+    let config:FrameworkConfigSettings = {
+      socialIcons: [
+        {imageFile: 'assets/social-fb-bw.png',alt: "Facebook", link: "http://www.facebook.com"}
+        ,{imageFile: 'assets/social-google-bw.png',alt: "Google", link: "http://www.google.com"}
+        ,{imageFile: 'assets/social-twitter-bw.png',alt: "Twitter", link: "http://www.twitter.com"}
+      ]
+      ,showStatusBarBreakpoint: 800
+      ,showLanguageSelector: true
+      ,showUserControls: true
+      ,showStatusBar: true
+    };
+
+    appConfigService.configure( config );
+  }
 }
