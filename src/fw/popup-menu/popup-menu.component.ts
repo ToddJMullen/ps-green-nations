8c1ecdb1707943c892a81dba6fd214ebd68d86f8
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-04 19:45:18 -0400

    Generate base PopupMenu component

---
+++ src/fw/popup-menu/popup-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fw-popup-menu',
+  templateUrl: './popup-menu.component.html',
+  styleUrls: ['./popup-menu.component.css']
+})
+export class PopupMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
