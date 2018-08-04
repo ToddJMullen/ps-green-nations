b5186c6737853f57a250a51ab2eeda4174bc9c96
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-04 13:55:39 -0400

    MenuComponent, Move into new menus/ folder

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 //custom
 import { FrameworkBodyComponent } from './framework-body/framework-body.component';
 import { ContentComponent } from './content/content.component';
-import { MenuComponent } from './menu/menu.component';
+import { MenuComponent } from './menus/menu/menu.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { FrameworkConfigService } from './services/framework-config.service';
 import { TopBarComponent } from './top-bar/top-bar.component';
