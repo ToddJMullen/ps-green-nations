09ad14bccf7b7466692706b257087a0573169526
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-04 19:14:10 -0400

    AppMenu, Update / add application routes

--- src/app/app.menu.ts
+++ src/app/app.menu.ts
@@ -1,25 +1,79 @@
-import { MenuItem } from "../fw/services/menu.service";
+import { MenuItem } from "../fw/menus/menu-item/menu-item.iface";
 
 export let initialMenuItems: Array<MenuItem> = [
-	{
-		text: "Dashboard"
-		,icon: "glyphicon-dashboard"
-		,route: "/dashboard"
-		,subMenu: null
-
-	}
-	,{
-		text: "Countries"
-		,icon: "glyphicon-flag"
-		,route: "/countries"
-		,subMenu: null
-
-	}
-	,{
-		text: "Settings"
-		,icon: "glyphicon-wrench"
-		,route: "/settings"
-		,subMenu: null
-
-	}
-]
+    {
+        text: 'Dashboard',
+        icon: 'glyphicon-dashboard',
+        route: 'dashboard',
+        subMenu: null
+    },
+    {
+        text: 'Countries',
+        icon: 'glyphicon-flag',
+        route: null,
+        subMenu: [
+            {
+                text: 'Select',
+                icon: 'glyphicon-expand',
+                route: null,
+                subMenu: [
+                    {
+                        text: 'USA',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/USA',
+                        subMenu: null
+                    },
+                    {
+                        text: 'India',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/India',
+                        subMenu: null
+                    },
+                    {
+                        text: 'Switzerland',
+                        icon: 'glyphicon-flag',
+                        route: 'country-detail/Switzerland',
+                        subMenu: null
+                    }
+                ]
+            },
+            {
+                text: 'Top 3',
+                icon: 'glyphicon-flag',
+                route: 'country-list/3',
+                subMenu: null
+            },
+            {
+                text: 'Top 10',
+                icon: 'glyphicon-flag',
+                route: 'country-list/10',
+                subMenu: null
+            },
+            {
+                text: 'All',
+                icon: 'glyphicon-flag',
+                route: 'country-list/0',
+                subMenu: null
+            }
+        ],
+    },
+    {
+        text: 'Maintenance',
+        icon: 'glyphicon-wrench',
+        route: null,
+        subMenu: [
+            {
+                text: 'Country List',
+                icon: 'glyphicon-th-list',
+                route: 'country-maint',
+                subMenu: null
+            },
+            {
+                text: 'Settings',
+                icon: 'glyphicon-cog',
+                route: 'settings',
+                subMenu: null
+            }
+        ]
+    }
+];
