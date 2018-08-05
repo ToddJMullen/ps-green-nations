2a96de378ddd00494749854307f881a115693319
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-05 19:10:59 -0400

    SignIn comp, import FwUserApi & Router and implment onSubmit()

--- src/fw/users/sign-in/sign-in.component.ts
+++ src/fw/users/sign-in/sign-in.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FwUserApi } from '../user-api';
+import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+
 @Component({
   selector: 'fw-sign-in',
   templateUrl: './sign-in.component.html',
@@ -11,9 +15,34 @@ export class SignInComponent implements OnInit {
   formError     = false;
   
 
-  constructor() { }
+  constructor(
+    private userApi: FwUserApi
+    ,private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSubmit( signInForm: NgForm ){
+    if( signInForm.valid ){
+      console.log("submitting...", signInForm );
+      this.submitting = true;
+      this.formError = null;
+
+      this.userApi.signIn(
+         signInForm.value.username
+        ,signInForm.value.password
+        ,signInForm.value.rememberMe
+      )
+      .subscribe( rsp => {
+        console.log("Sign Rsp:", rsp );
+        this.router.navigate(["/authenticated"])
+      }, err => {
+        console.error("Sign In Failed:", err );
+        this.submitting = false;
+        this.formError = err;
+      } )
+    }
   }
 
 }
