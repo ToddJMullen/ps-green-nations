d2d521e4e496628d3a1dea83b066cb78fd5867c7
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-04 20:57:01 -0400

    Setup initial popup menu items

--- src/fw/menus/menu-item/menu-item.component.ts
+++ src/fw/menus/menu-item/menu-item.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostBinding, HostListener } from '@angular/core';
 
 // import { MenuItem } from '../../services/menu.service';//og location
 //I chose the 'separate file' solution
 import { MenuItem } from './menu-item.iface';
 import { MenuService } from '../../services/menu.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'fw-menu-item',
@@ -13,14 +14,57 @@ import { MenuService } from '../../services/menu.service';
 export class MenuItemComponent implements OnInit {
 
   @Input() item = <MenuItem>null;
-  //Issue 2034, warnings when using type declarariont of an interface not declared in own file
-  
+  //Issue 2034, ^^^^^^^^^ warnings when using type declarariont of an interface not declared in own file
+  @HostBinding("class.parent-is-popup")
+  @Input() parentIsPopup  = true;
+
+  isActiveRoute   = false;
+
+  mouseInPopup    = false;
+  mouseInItem     = false;
+
+  popupTop        = 37;
+  popupLeft       = 0;
 
   constructor(
-    private menuService: MenuService
+    private router:Router
+    ,private menuService: MenuService
   ) { }
 
   ngOnInit() {
   }
-  
+
+  onPopupMouseEnter(event){
+    if( !this.menuService.isVertical ){
+      this.mouseInPopup = true;
+    }
+  }
+
+  onPopupMouseLeave(event){
+    if( !this.menuService.isVertical ){
+      this.mouseInPopup = false;
+    }
+  }
+ 
+  @HostListener("mouseleave",["$event"])
+  onMouseLeave(event):void{
+    if( !this.menuService.isVertical ){
+      this.mouseInItem = false;
+    }
+  }
+
+  @HostListener("mouseenter")
+  onMouseEnter():void{
+    if( !this.menuService.isVertical ){
+      if( this.item.subMenu ){
+        this.mouseInItem = true;
+        if( this.parentIsPopup ){
+          this.popupLeft  = 160;
+          this.popupTop   = 0;
+        }
+      }
+    }
+  }
+
+
 }
--- src/fw/popup-menu/popup-menu.component.ts
+++ src/fw/popup-menu/popup-menu.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 import { MenuService } from '../services/menu.service';
+import { MenuItem } from '../menus/menu-item/menu-item.iface';
 
 @Component({
   selector: 'fw-popup-menu',
@@ -8,6 +9,8 @@ import { MenuService } from '../services/menu.service';
   styleUrls: ['./popup-menu.component.css']
 })
 export class PopupMenuComponent implements OnInit {
+
+  @Input() menu:Array<MenuItem>;
 
   constructor(
     private menuService: MenuService
