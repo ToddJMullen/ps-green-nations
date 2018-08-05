093434d1fa107d4b024dd0976132542e01148605
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-05 19:08:13 -0400

    Define FwUserApi interface for required user service signature(s)

---
+++ src/fw/users/user-api.ts
@@ -0,0 +1,12 @@
+import { Observable } from "rxjs/Observable";
+
+
+export abstract class FwUserApi{
+	//we define the method signatures as an interface expected by the Fw Users package
+	// The API should be implemented by the app and injected back into the Framework
+	signIn: (username:string, password: string, rememberMe: boolean) => Observable<any>;
+	// register
+	// signOut
+	//resetPwd
+	//etc...
+}
