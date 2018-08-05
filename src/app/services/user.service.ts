d50401d4eba505fa22e8fc0735d92d6a91331919
ToddJMullen <ToddJ.Mullen@Gmail.com>
2018-08-05 19:53:22 -0400

    UserService,  Add disabled code for testing error/ failure - Correct promperty name typo isAuthenticate => isAuthenticated

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Observable";
 import { delay } from "rxjs/operators/delay";
 import "rxjs/add/observable/of";
+import "rxjs/add/observable/throw";
 
 import { FwUserApi } from "../../fw/users/user-api";
 
 @Injectable()
 export class UserService implements FwUserApi{
-	isAuthenticate: true;
+	isAuthenticated: true;
 
 	constructor(){}
 
 	signIn( username:string, pwd:string, rememberMe:boolean ): Observable<any>{
 		console.log(`UserService::signIn(): ${username}, ${pwd}, ${rememberMe}`)
-		this.isAuthenticate = true;
-		return Observable.of({}).pipe( delay(1000) );
+		this.isAuthenticated = true;
+		return Observable.of({}).pipe(delay(1000))
+		// return Observable.throw("Invalid User Name / Password")
+		// ^^^ for testing
 	}
 }
